fda21755327c32c0b9b05dbd869559d5b5223b7d
Dok6n <ehrbs2018@gmail.com>
2022-04-25 03:27:11 +0900

    feat: Add deploy.sh s3 & cloudfront

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ -f .env ] ;  then 
+  export  $( echo $( cat .env | sed ' s/#.*//g ' | xargs )  | envsubst )
+fi
+
+
+# build
+react-scripts build
+
+# s3 upload
+aws s3 sync ./build s3://$S3_BUCKET_NAME --profile=$AWS_CLI_PROFILE
+
+# deploy cloudfront
+aws cloudfront create-invalidation --profile=dok6n --distribution-id $DISTRIBUTION_ID --paths / /index.html /service-worker.js /manifest.json /favicon.ico

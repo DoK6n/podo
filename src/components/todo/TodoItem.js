92fd82f33ba556571cace49e47a9c409421e06b5
Dok6n <ehrbs2018@gmail.com>
2022-04-30 14:52:19 +0900

    feat: Create edit icon and hide remove icon

--- src/components/todo/TodoItem.js
+++ src/components/todo/TodoItem.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import { CheckIcon, ItemText, RemoveIcon, TodoItemBlock } from '../../styles/todo';
+import React, { useState } from 'react';
+import { CheckIcon, EditIcon, ItemText, RemoveIcon, TodoItemBlock } from '../../styles/todo';
 import { FcEmptyTrash } from 'react-icons/fc';
+import { BiEdit } from 'react-icons/bi';
 import { IoWaterOutline, IoWaterSharp } from 'react-icons/io5';
+
 import { useTodoDispatch } from '../../hooks/todoContext';
 import { Draggable } from 'react-beautiful-dnd';
 
 function TodoItem({ id, text, done, index }) {
+  const [edited, setEdited] = useState(false);
+
   const dispatch = useTodoDispatch();
   const onRemoveitem = () => {
     dispatch({ type: 'REMOVE_ITEM', id });
@@ -13,6 +17,10 @@ function TodoItem({ id, text, done, index }) {
 
   const onToggleItem = () => {
     dispatch({ type: 'TOGGLE_ITEM', id });
+  };
+
+  const onEditItem = () => {
+    setEdited(state => !state);
   };
 
   return (
@@ -23,9 +31,14 @@ function TodoItem({ id, text, done, index }) {
             {done === false ? <IoWaterOutline size={30} /> : <IoWaterSharp size={30} />}
           </CheckIcon>
           <ItemText {...provided.dragHandleProps}>{text}</ItemText>
-          <RemoveIcon onClick={onRemoveitem}>
-            <FcEmptyTrash />
-          </RemoveIcon>
+          <EditIcon onClick={onEditItem}>
+            <BiEdit />
+          </EditIcon>
+          {edited === true ? (
+            <RemoveIcon onClick={onRemoveitem}>
+              <FcEmptyTrash />
+            </RemoveIcon>
+          ) : null}
         </TodoItemBlock>
       )}
     </Draggable>

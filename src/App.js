34d2ae00f440782aee56aefbcb9b8745ab3f3257
Dok6n <ehrbs2018@gmail.com>
2022-05-05 16:21:02 +0900

    feat: Add route TodoTemplate

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { GlobalStyle } from './styles/global';
 import { TodoTemplate } from './components/todo';
 import { Header } from './components/base';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Header />
-      <TodoTemplate />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<TodoTemplate />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }

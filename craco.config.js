a1ca9c73deb9a2801915e62d08c3ea0cb3c22f1e
Dok6n <ehrbs2018@gmail.com>
2022-05-05 18:20:47 +0900

    chore(craco): Update alias @pages

--- craco.config.js
+++ craco.config.js
@@ -5,6 +5,7 @@ module.exports = {
     alias: {
       '@': path.resolve(__dirname, 'src/'),
       '@assets': path.resolve(__dirname, 'src/assets'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
       '@components': path.resolve(__dirname, 'src/components'),
       '@styles': path.resolve(__dirname, 'src/styles'),
       '@hooks': path.resolve(__dirname, 'src/hooks'),

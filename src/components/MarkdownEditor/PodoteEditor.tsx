c1987ca33a466b71e94e0110ffa4334f457c3ee2
Dok6n <ehrbs2018@gmail.com>
2022-06-05 19:13:11 +0900

    feat: Add custom wysiwyg editor using remirror

---
+++ src/components/MarkdownEditor/PodoteEditor.tsx
@@ -0,0 +1,165 @@
+import React from 'react';
+import {
+  componentsStyledCss,
+  coreStyledCss,
+  extensionBlockquoteStyledCss,
+  extensionCalloutStyledCss,
+  extensionCodeBlockStyledCss,
+  extensionEmojiStyledCss,
+  extensionFileStyledCss,
+  extensionGapCursorStyledCss,
+  extensionImageStyledCss,
+  extensionListStyledCss,
+  extensionMentionAtomStyledCss,
+  extensionNodeFormattingStyledCss,
+  extensionPlaceholderStyledCss,
+  extensionPositionerStyledCss,
+  extensionTablesStyledCss,
+  extensionWhitespaceStyledCss,
+  extensionYjsStyledCss,
+  themeStyledCss,
+} from '@remirror/styles/styled-components';
+import { EditorComponent, Remirror, ThemeProvider, useActive, useChainedCommands, useRemirror } from '@remirror/react';
+import {
+  BoldExtension,
+  CalloutExtension,
+  HeadingExtension,
+  HistoryExtension,
+  ItalicExtension,
+  UnderlineExtension,
+  ImageExtension,
+  DropCursorExtension,
+} from 'remirror/extensions';
+import styled, { css } from 'styled-components';
+
+// editor theme styling
+const extensionCountStyledCss: ReturnType<typeof css> = css`
+  .remirror-editor span.remirror-max-count-exceeded {
+    background-color: var(--rmr-hue-red-4);
+  }
+`;
+
+const PodoteThemeStyledCss: ReturnType<typeof css> = css`
+  --podote-color-heading-text: #c9d1d9;
+  --podote-color-normal-text: #abb2bf;
+
+  width: 100%;
+  padding: 15px 0 15px 0;
+  color: var(--podote-color-normal-text);
+  strong,
+  b,
+  strong *,
+  b * {
+    // styled-reset으로 인해 font: inherit이 적용된 태그에 bold, italic 재정의
+    font-weight: bold;
+  }
+  em,
+  i,
+  em *,
+  i {
+    font-style: italic;
+  }
+
+  .remirror-theme h1,
+  .remirror-theme h2,
+  .remirror-theme h3,
+  .remirror-theme h4,
+  .remirror-theme h5,
+  .remirror-theme h6 {
+    color: var(--podote-color-heading-text);
+  }
+  .remirror-editor-wrapper {
+    padding-top: 10px;
+  }
+  .remirror-theme .ProseMirror {
+    box-shadow: transparent 0px 0px 0px 0.1em;
+  }
+  .remirror-theme {
+    --rmr-color-selection-background: var(--rmr-color-outline);
+    --rmr-color-selection-shadow: inherit;
+    --rmr-color-selection-text: var(--podote-color-text);
+    --rmr-color-selection-caret: inherit;
+  }
+`;
+
+const PodoteTheme: ReturnType<typeof styled.div> = styled.div`
+  ${componentsStyledCss}
+  ${coreStyledCss}
+  ${extensionBlockquoteStyledCss}
+  ${extensionCalloutStyledCss}
+  ${extensionCodeBlockStyledCss}
+  ${extensionCountStyledCss}
+  ${extensionEmojiStyledCss}
+  ${extensionFileStyledCss}
+  ${extensionGapCursorStyledCss}
+  ${extensionImageStyledCss}
+  ${extensionListStyledCss}
+  ${extensionMentionAtomStyledCss}
+  ${extensionNodeFormattingStyledCss}
+  ${extensionPlaceholderStyledCss}
+  ${extensionPositionerStyledCss}
+  ${extensionTablesStyledCss}
+  ${extensionWhitespaceStyledCss}
+  ${extensionYjsStyledCss}
+  ${themeStyledCss}
+  ${PodoteThemeStyledCss}
+`;
+
+const Menu = () => {
+  const chain = useChainedCommands();
+  const active = useActive();
+  return (
+    <button
+      onClick={() => {
+        chain //
+          .toggleBold()
+          .focus()
+          .run();
+      }}
+      style={{ fontWeight: active.bold() ? 'bold' : undefined }}
+    >
+      B
+    </button>
+  );
+};
+
+function PodoteEditor() {
+  const extensions = () => [
+    new BoldExtension(),
+    new ItalicExtension(),
+    new UnderlineExtension(),
+    new HeadingExtension(),
+    new CalloutExtension({ defaultType: 'warn' }),
+    new HistoryExtension(),
+    new ImageExtension(),
+    new DropCursorExtension({ color: '#7963d2' }),
+  ];
+  const { manager, state, setState } = useRemirror({
+    extensions: extensions,
+    content: `<p>I love <b>Remirror</b></p>
+      <p><img src="https://dummyimage.com/200x80/479e0c/fafafa"></p>
+    `,
+    selection: 'end',
+    stringHandler: 'html',
+  });
+
+  const onChangeState = (parameter: any) => {
+    // Update the state to the latest value.
+    setState(parameter.state);
+    console.log(state);
+    console.log(parameter);
+  };
+
+  return (
+    <PodoteTheme id={'podote-theme'}>
+      <ThemeProvider>
+        <Remirror manager={manager} initialContent={state} onChange={onChangeState}>
+          <Menu />
+          <EditorComponent />
+        </Remirror>
+      </ThemeProvider>
+    </PodoteTheme>
+  );
+}
+
+export default PodoteEditor;

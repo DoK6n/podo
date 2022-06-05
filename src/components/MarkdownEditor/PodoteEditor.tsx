c856e221c414cb500f15f41f7b4950fbd57b60dc
Dok6n <ehrbs2018@gmail.com>
2022-06-05 19:13:11 +0900

    feat: Update podote editor customizing
    - Podote theme style code 분리
    - Extension 추가
    - 관리할 컨텐츠 구조를 html에서 json으로 변경
    - 수정모드/비수정모드에 따른 editor view 추가
    - 상태관리 적용

--- src/components/MarkdownEditor/PodoteEditor.tsx
+++ src/components/MarkdownEditor/PodoteEditor.tsx
@@ -28,97 +28,12 @@ import {
   UnderlineExtension,
   ImageExtension,
   DropCursorExtension,
+  StrikeExtension,
+  HorizontalRuleExtension,
 } from 'remirror/extensions';
-import styled, { css } from 'styled-components';
-
-// editor theme styling
-const extensionCountStyledCss: ReturnType<typeof css> = css`
-  .remirror-editor span.remirror-max-count-exceeded {
-    background-color: var(--rmr-hue-red-4);
-  }
-`;
-
-export const extensionCalloutStyledCss: ReturnType<typeof css> = css`
-  .remirror-editor div[data-callout-type] {
-    display: flex;
-    margin-left: 0;
-    margin-right: 0;
-    margin-block-start: 1em;
-    margin-block-end: 1em;
-    margin-inline-end: 40px;
-    padding: 10px;
-    border-left: 0.25em solid transparent;
-    border-top-right-radius: 4px;
-    border-bottom-right-radius: 4px;
-  }
-  .remirror-editor div[data-callout-type] > :not(.remirror-callout-emoji-wrapper) {
-    margin-left: 8px;
-    flex-grow: 1;
-  }
-  .remirror-editor div[data-callout-type='info'] {
-    background: #3298dc11;
-    border-left-color: #3298dc;
-  }
-  .remirror-editor div[data-callout-type='warning'] {
-    background: #ffdd5711;
-    border-left-color: #ffdd57;
-  }
-  .remirror-editor div[data-callout-type='error'] {
-    background: #f1466811;
-    border-left-color: #f14668;
-  }
-  .remirror-editor div[data-callout-type='success'] {
-    background: #48c77411;
-    border-left-color: #48c774;
-  }
-  .remirror-editor div[data-callout-type='blank'] {
-    background: #483d6b5c;
-    border-left-color: #483d6b;
-  }
-`;
-
-const PodoteThemeStyledCss: ReturnType<typeof css> = css`
-  --podote-color-heading-text: #c9d1d9;
-  --podote-color-normal-text: #abb2bf;
-
-  width: 100%;
-  padding: 15px 0 15px 0;
-  color: var(--podote-color-normal-text);
-  strong,
-  b,
-  strong *,
-  b * {
-    // styled-reset으로 인해 font: inherit이 적용된 태그에 bold, italic 재정의
-    font-weight: bold;
-  }
-  em,
-  i,
-  em *,
-  i {
-    font-style: italic;
-  }
-
-  .remirror-theme h1,
-  .remirror-theme h2,
-  .remirror-theme h3,
-  .remirror-theme h4,
-  .remirror-theme h5,
-  .remirror-theme h6 {
-    color: var(--podote-color-heading-text);
-  }
-  .remirror-editor-wrapper {
-    padding-top: 10px;
-  }
-  .remirror-theme .ProseMirror {
-    box-shadow: transparent 0px 0px 0px 0.1em;
-  }
-  .remirror-theme {
-    --rmr-color-selection-background: var(--rmr-color-outline);
-    --rmr-color-selection-shadow: inherit;
-    --rmr-color-selection-text: var(--podote-color-text);
-    --rmr-color-selection-caret: inherit;
-  }
-`;
+import styled from 'styled-components';
+import { extensionCalloutStyledCss, extensionCountStyledCss, podoteThemeStyledCss } from 'styles';
+import { useTodoStore } from 'hooks';
 
 const PodoteTheme: ReturnType<typeof styled.div> = styled.div`
   ${componentsStyledCss}
@@ -140,7 +55,7 @@ const PodoteTheme: ReturnType<typeof styled.div> = styled.div`
   ${extensionWhitespaceStyledCss}
   ${extensionYjsStyledCss}
   ${themeStyledCss}
-  ${PodoteThemeStyledCss}
+  ${podoteThemeStyledCss}
 `;
 
 const Menu = () => {
@@ -161,38 +76,63 @@ const Menu = () => {
   );
 };
 
-function PodoteEditor() {
+interface Props {
+  id: string;
+  editable: boolean;
+  content?: string | any | Object;
+  setTestOnlyContentJSON?: any | Object;
+}
+
+function PodoteEditor({ id, editable, content, setTestOnlyContentJSON }: Props) {
+  const { editItemText } = useTodoStore();
+
   const extensions = () => [
     new BoldExtension(),
     new ItalicExtension(),
+    new StrikeExtension(),
     new UnderlineExtension(),
     new HeadingExtension(),
     new CalloutExtension({ defaultType: 'blank' }),
     new HistoryExtension(),
     new ImageExtension(),
     new DropCursorExtension({ color: '#7963d2', width: 4 }),
+    new HorizontalRuleExtension(),
   ];
+
+  const initialContent = {
+    type: 'doc',
+    content: [
+      { type: 'heading', attrs: { level: 1 }, content: [{ type: 'text', text: 'Hello world' }] },
+      {
+        type: 'paragraph',
+        content: [
+          { type: 'text', text: 'Hello ' },
+          { type: 'text', marks: [{ type: 'italic' }], text: 'word' },
+        ],
+      },
+    ],
+  };
+
   const { manager, state, setState } = useRemirror({
     extensions: extensions,
-    content: `<p>I love <b>Remirror</b></p>
-      <p><img src="https://dummyimage.com/200x80/479e0c/fafafa"></p>
-    `,
+    content: content ? content : initialContent,
     selection: 'end',
-    stringHandler: 'html',
   });
 
   const onChangeState = (parameter: any) => {
     // Update the state to the latest value.
     setState(parameter.state);
-    console.log(state);
-    console.log(parameter);
+
+    // editor testing page only
+    if (setTestOnlyContentJSON) setTestOnlyContentJSON(parameter.state.doc);
+    else editItemText({ id, content: parameter.state.doc }); // main page update content object
   };
 
   return (
     <PodoteTheme id={'podote-theme'}>
       <ThemeProvider>
-        <Remirror manager={manager} initialContent={state} onChange={onChangeState}>
-          <Menu />
+        <Remirror manager={manager} initialContent={state} onChange={onChangeState} editable={editable}>
+          {editable ? <Menu /> : null}
           <EditorComponent />
         </Remirror>
       </ThemeProvider>

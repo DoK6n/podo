2367b15957d72f6516987a370fcff6ce770de0c6
Dok6n <ehrbs2018@gmail.com>
2022-06-20 18:09:16 +0900

    refactor: Add export heading svg files

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -1,1 +1,7 @@
 export { ReactComponent as Logo } from './logo.svg';
+export { ReactComponent as Heading1 } from './Heading1.svg';
+export { ReactComponent as Heading2 } from './Heading2.svg';
+export { ReactComponent as Heading3 } from './Heading3.svg';
+export { ReactComponent as Heading4 } from './Heading4.svg';
+export { ReactComponent as Heading5 } from './Heading5.svg';
+export { ReactComponent as Heading6 } from './Heading6.svg';

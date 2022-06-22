6affba4bc4b649c922bfce48d910f734ba8a1a56
Dok6n <ehrbs2018@gmail.com>
2022-06-22 16:16:34 +0900

    style: Add custom callout icons

--- src/assets/index.tsx
+++ src/assets/index.tsx
@@ -5,3 +5,8 @@ export { ReactComponent as Heading3 } from './Heading3.svg';
 export { ReactComponent as Heading4 } from './Heading4.svg';
 export { ReactComponent as Heading5 } from './Heading5.svg';
 export { ReactComponent as Heading6 } from './Heading6.svg';
+export { ReactComponent as CalloutBlank } from './Callout_Blank.svg';
+export { ReactComponent as CalloutError } from './Callout_Error.svg';
+export { ReactComponent as CalloutInfo } from './Callout_Info.svg';
+export { ReactComponent as CalloutSuccess } from './Callout_Success.svg';
+export { ReactComponent as CalloutWarn } from './Callout_Warn.svg';

0a449c35dbdfcdbf8cdd8c5f96b993a13ae0b2ef
Dok6n <ehrbs2018@gmail.com>
2022-08-01 16:44:44 +0900

    style: `Update` base style

--- src/styles/base/index.tsx
+++ src/styles/base/index.tsx
@@ -3,18 +3,28 @@ import { css } from 'styled-components';
 export const headerStyledCss: ReturnType<typeof css> = css`
   background: #5c4b8c40;
   width: auto;
-  height: 50px;
+  height: auto;
   display: flex;
   justify-content: center;
   flex-direction: column;
 `;
-export const navStyledCss: ReturnType<typeof css> = css`
+export const logoNavStyledCss: ReturnType<typeof css> = css`
   width: auto;
   height: 50px;
   display: flex;
   flex-direction: row;
   align-items: baseline;
   justify-content: space-around;
+`;
+
+export const navStyledCss: ReturnType<typeof css> = css`
+  width: auto;
+  height: 1.3em;
+  display: flex;
+  flex-direction: row;
+  align-items: baseline;
+  justify-content: flex-end;
+  padding: 0 10% 0 10%;
 `;
 
 export const podoteColorsStyledCss: ReturnType<typeof css> = css`

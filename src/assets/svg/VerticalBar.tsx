bec6805525c74adc542c03e9067f3719d54dc209
Dok6n <ehrbs2018@gmail.com>
2022-08-02 13:44:16 +0900

    style: `Separate` svg files into folder

---
+++ src/assets/svg/VerticalBar.tsx
@@ -0,0 +1,3 @@
+import { TbMinusVertical } from 'react-icons/tb';
+
+export const VerticalBarIcon = () => <TbMinusVertical style={{ color: '#605679' }} viewBox={'0 -3 24 24'} />;
---
+++ src/assets/svg/index.tsx
@@ -0,0 +1,13 @@
+export { ReactComponent as Logo } from './logo.svg';
+export { ReactComponent as Heading1 } from './Heading1.svg';
+export { ReactComponent as Heading2 } from './Heading2.svg';
+export { ReactComponent as Heading3 } from './Heading3.svg';
+export { ReactComponent as Heading4 } from './Heading4.svg';
+export { ReactComponent as Heading5 } from './Heading5.svg';
+export { ReactComponent as Heading6 } from './Heading6.svg';
+export { ReactComponent as CalloutBlank } from './Callout_Blank.svg';
+export { ReactComponent as CalloutError } from './Callout_Error.svg';
+export { ReactComponent as CalloutInfo } from './Callout_Info.svg';
+export { ReactComponent as CalloutSuccess } from './Callout_Success.svg';
+export { ReactComponent as CalloutWarn } from './Callout_Warn.svg';
+export * from './VerticalBar';

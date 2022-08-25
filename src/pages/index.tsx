cbcc6c43572379f23ab7840050f68d460becedf2
Dok6n <ehrbs2018@gmail.com>
2022-08-25 17:02:03 +0900

    refactor: `Update` directory name

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
 export { default as TodoPage } from './Todo';
 export { default as TrashBinPage } from './TrashBin';
-export { default as MDPlaygroundPage } from './MDPlayground';
+export { default as MDPlaygroundPage } from './MarkdownPlayground';
 export { default as PodoteEditorPage } from './PodoteEditor';

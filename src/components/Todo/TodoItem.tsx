575f1b1cebc9abcc66339d589a0f0693bcbcc901
Dok6n <ehrbs2018@gmail.com>
2022-08-30 19:14:02 +0900

    design: `Update` edit, remove icons
    - 로그인시 디비에 저장하기 위해 기존에 수정아이콘과 동일하게 쓰던 저장 버튼을 저장 아이콘으로 변경
    - todo 체크 아이콘 사이즈 조절

--- src/components/Todo/TodoItem.tsx
+++ src/components/Todo/TodoItem.tsx
@@ -9,14 +9,15 @@ import {
   todoItemBlockStyledCss,
 } from 'styles';
 import { PodoteEditor } from 'components';
-import { FcEmptyTrash } from 'react-icons/fc';
 import { BiEdit } from 'react-icons/bi';
+import { IoMdCloudDone } from 'react-icons/io';
 import { IoWaterOutline, IoWaterSharp } from 'react-icons/io5';
 import { MdDragIndicator } from 'react-icons/md';
 import { Draggable } from 'react-beautiful-dnd';
 import { useTodoStore, useTodoTrashBinStore } from 'lib/stores';
 import styled, { TodoStylesProps } from 'styled-components';
 import { RemirrorJSON } from 'remirror';
+import { BsTrashFill } from 'react-icons/bs';
 
 const TodoItemBlock = styled.section<TodoStylesProps>`
   ${todoItemBlockStyledCss}
@@ -80,18 +81,16 @@ function TodoItem({ id, content, done, editable, index }: Props) {
               <MdDragIndicator />
             </DragHandleIcon>
             <CheckIcon onClick={onToggleItem} done={done}>
-              {done === false ? <IoWaterOutline size={30} /> : <IoWaterSharp size={30} />}
+              {done === false ? <IoWaterOutline size={25} /> : <IoWaterSharp size={25} />}
             </CheckIcon>
           </ItemBlockLeftIconWrapper>
           <ItemText done={done} editable={editable}>
             <PodoteEditor id={id} editable={editable} content={content} editorType={'TODO_ITEM'} />
           </ItemText>
-          <EditIcon onClick={onEditItem}>
-            <BiEdit />
-          </EditIcon>
+          <EditIcon onClick={onEditItem}>{editable ? <IoMdCloudDone /> : <BiEdit />}</EditIcon>
           {editable ? (
             <RemoveIcon onClick={onRemoveitem}>
-              <FcEmptyTrash />
+              <BsTrashFill />
             </RemoveIcon>
           ) : null}
         </TodoItemBlock>

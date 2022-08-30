5d7c472899aaa37b8461155b8db8fef5c72d8725
Dok6n <ehrbs2018@gmail.com>
2022-08-30 16:18:43 +0900

    feat: `Add` todo page login validation
    zustand의 auth store를 통해 로그인 여부 검증
    - auth store에 로컬스토리지 옵션 추가되있음

--- src/pages/Todo/index.tsx
+++ src/pages/Todo/index.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { TodoTemplate } from 'components';
+import { authMode, useAuthStore } from 'lib/stores';
+import { useNavigate } from 'react-router-dom';
 
 function TodoPage() {
-  return (
-    <>
-      <TodoTemplate />
-    </>
-  );
+  const { currentUserInfo, mode } = useAuthStore();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (mode === authMode.GUEST_MODE && currentUserInfo === null) return navigate('/');
+  }, []);
+
+  return <>{mode === authMode.LOGIN_MODE && currentUserInfo !== null ? <TodoTemplate /> : null}</>;
 }
 
 export default TodoPage;
